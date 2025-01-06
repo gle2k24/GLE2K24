0f5d8722e820e78e89060400acdf798be3a55d60
gle2k24 <atenasantinelle@gmail.com>
2025-01-06 11:41:03 -0300

    Add files via upload

---
+++ 10/projetopython/teste02.py
@@ -0,0 +1,14 @@
+nome = input("Qual o seu nome: ")
+idade = input("Qual a sua idade: ")
+print("seu nome:", nome, "sua idade: ", idade, )
+
+# propriedades numericas 
+
+num1 = float(input("Digite o primeiro numero: "))
+num2 = float(input("Digite o segundo numero: "))
+soma = num1 + num2
+diferença = num1 - num2
+produto = num1*num2
+print(f"A soma e:{soma }")
+print(f"A diferença e:{diferença}")
+print(f"O produto e:{produto}")
