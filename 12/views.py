c1eb9a1d1d15e12f3477c8ccb69b35733a0ad562
gle2k24 <atenasantinelle@gmail.com>
2025-01-27 10:46:52 -0300

    Add files via upload

---
+++ 12/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request,'index.html')
+
+def sobre(request):
+    return render(request,'sobre.html')
+
+def contato(request):
+    return render(request,'contato.html')
+
+def ajuda(request):
+    return render(request,'ajuda.html')
+
